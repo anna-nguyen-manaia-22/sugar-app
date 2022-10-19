9bb5e6ce4149e1ed20b8846789d191b006fadd13
anna-nguyen-manaia-22 <107183001+anna-nguyen-manaia-22@users.noreply.github.com>
2022-10-19 16:00:30 +1300

    feat: return full new record after add to db

--- server/db/db.js
+++ server/db/db.js
@@ -8,7 +8,15 @@ function addBS(data, db = connection) {
   return db('blood_sugar_values').insert(data)
 }
 
+function getBsById(id, db = connection) {
+  return db('blood_sugar_values')
+    .select()
+    .where('id', id)
+    .then((bloodSugars) => bloodSugars[0])
+}
+
 module.exports = {
   getBloodSugars,
   addBS,
+  getBsById,
 }
--- server/routes/blood-sugars.js
+++ server/routes/blood-sugars.js
@@ -3,30 +3,25 @@ const db = require('../db/db')
 
 const router = express.Router()
 
+const apiError = (err, res) => res.status(500).json({ message: err.message })
+
 router.get('/', (req, res) => {
   db.getBloodSugars()
     .then((results) => {
       res.json(results)
       return null
     })
-    .catch((err) => {
-      console.log(err)
-      res.status(500).json({ message: 'Somthing went wrong' })
-    })
+    .catch((err) => apiError(err, res))
 })
 
 router.post('/', (req, res) => {
   const data = req.body
   console.log('post payload', data)
   db.addBS(data)
-    .then((results) => {
-      res.json(results[0])
-      return null
-    })
-    .catch((err) => {
-      console.log(err)
-      res.status(500).json({ message: 'Failed to save sugar: ' + err.message })
-    })
+    .then((results) => results[0])
+    .then((id) => db.getBsById(id))
+    .then((result) => res.json(result))
+    .catch((err) => apiError(err, res))
 })
 
 module.exports = router
