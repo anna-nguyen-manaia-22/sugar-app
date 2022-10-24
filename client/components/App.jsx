09c62d70ae66f8afcf9cc14f7ed791d3613bc405
anna-nguyen-manaia-22 <107183001+anna-nguyen-manaia-22@users.noreply.github.com>
2022-10-25 11:37:41 +1300

    feat: create AddBS form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { Link, Route, Routes } from 'react-router-dom'
 
 import { listBSRequest } from '../actions'
 import ListBS from './ListBS'
+import AddBS from './AddBS'
 
 function App() {
   const bloodSugars = useSelector((state) => state.bloodSugars)
@@ -17,10 +18,12 @@ function App() {
       <h1>Welcome to My Sugar App!</h1>
       <Routes>
         <Route path="/list-BS" element={<ListBS />} />
+        <Route path="/add-BS" element={<AddBS />} />
       </Routes>
-      <br />
+
       <Link to="list-bs">My sugar list</Link>
       <br />
+      <Link to="/add-BS">Add Blood Sugar Record</Link>
     </div>
   )
 }
